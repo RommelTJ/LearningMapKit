1a802b283505a17512e5fb89c577226dea1e25f8
Rommel Rico <me@rommelrico.com>
2017-10-18 08:33:01 -0700

    Setting up the DestinationTableViewCell

---
+++ LearningMapKit/DestinationTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DestinationTableViewCell.swift
+//  LearningMapKit
+//
+//  Created by Rommel Rico on 10/18/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DestinationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LearningMapKit/ViewController.swift
+++ LearningMapKit/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var etaLabel: UILabel!
+    @IBOutlet weak var departureTimeLabel: UILabel!
+    @IBOutlet weak var arrivalTimeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func viewRouteTapped(_ sender: UIButton) {
+        // TODO.
     }
-
 
 }
 
